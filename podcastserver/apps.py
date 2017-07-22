6cbc94a7f84c32ff21046f534a06e932f7286346
technocake <technocake@komsys.org>
2017-07-22 20:55:25 +0200

    First start off the django-implementation of a podcastserver for srib.
    Studentradioen i Bergen is getting a new podcast-server implementation.
    The goal is to make a new server written in python and django that will:
    
     - Be more robust, and scale over time (when db and activity increase)
     - Ease administration of podcasts.
    
    To do this we are going to:
      - use cache -- serving the rss from cache.
      - use podgen to generate RSS
    	 (lib that is made by studentmediene i Trondheim)
    
      - allow for a administration model that lets one user (say
    	our net-editor) to administrate all podcasts.
    
    - technocake

---
+++ podcastserver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PodcastserverConfig(AppConfig):
+    name = 'podcastserver'
---
+++ podcastserver/models.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+
+    models.py
+    ¨¨¨¨¨¨¨¨¨
+    
+    Modeller for databasen i podkastserveren.
+"""
+from django.db import models
+from django.conf.global_settings import LANGUAGES
+from podgen.category import Category
+
+CATEGORIES = ((c,c) for c in Category._categories.keys())
+""" Getting iTunes Store supported categories from the podgen module.
+    
+    See list here: https://github.com/tobinus/python-podgen/blob/master/podgen/category.py#L39-L67
+
+    Django needs choices in the following tuple format:
+    ( 
+        ('Choice1': "Choice one human readable"),
+        ('Choice1': "Choice one human readable")
+    )
+
+"""
+
+class Person(models.Model):
+    u"""
+        Person = <Name, Email>
+    """
+    fullname = models.CharField(max_length=128)
+    email = models.EmailField(max_length=70,blank=True)
+
+
+class Podcast(models.Model):
+    u"""Description: The info on a podcast.
+
+    Denne klassen brukes for å administrere selve
+    podcasten. Episodene har egne datafelt, som man
+    finner i Episode-klassen.
+    """
+
+    name = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=250)
+    description = models.TextField()
+    website = models.URLField(null=True, max_length=100)
+    explicit = models.BooleanField()
+    authors = models.ManyToManyField(Person, related_name='authors', blank=True)
+
+
+    language = models.CharField(max_length=7, choices=LANGUAGES, default='nb')
+    """ Codes supported seems to be 21. 
+        
+    For a list see here:
+    http://www.ibabbleon.com/iOS-Language-Codes-ISO-639.html
+    """
+
+    category = models.CharField(max_length=20, choices=CATEGORIES)
+    image_url = models.URLField(null=True, max_length=500)
+    """ Details about image specs below.
+
+    from the podgen documentation: 
+
+        The URL of the artwork for this podcast. iTunes prefers square images 
+        that are at least 1400x1400 pixels.
+        Podcasts with an image smaller than this are not eligible to be 
+        featured on the iTunes Store.
+    
+        iTunes supports images in JPEG and PNG formats with an RGB color space 
+        (CMYK is not supported). The URL must end in ”.jpg” or ”.png”; if they 
+        don’t, a NotSupportedByItunesWarning will be issued.
+
+            - http://podgen.readthedocs.io/en/latest/user/basic_usage_guide/part_1.html#image
+    """
+
+    owner = models.ForeignKey(Person)
+    """<itunes:owner>  - the legal owner of the podcast.
+
+    should be: Studentradioen i Bergen - kontakt@srib.no
+    """
+
+
+    def __str__(self):
+        """
+        Denne magiske funksjonen bestemmer hvordan 
+        print(podcast) vil se ut.
+
+        Eksempel:
+            <Podcast Skumma Kultur>
+
+        """
+        return "<Podcast %s>" % self.title
+
+
