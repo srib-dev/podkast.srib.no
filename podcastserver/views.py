10d7c648b1f61f005c57ad707451ff20ca3c6e57
Markus Pedersen <markus@markus2.com>
2019-02-15 23:41:36 +0100

    Reverted test

--- podcastserver/views.py
+++ podcastserver/views.py
@@ -23,7 +23,7 @@ def teknisksjef(request):
 
 def index(request):
     # Henter ut kun programmer som har publish satt til True.
-    publiserte_programmer = ProgramInfo.objects.filter(publish=False)
+    publiserte_programmer = ProgramInfo.objects.filter(publish=True)
     return render(request, 'index.htm', dict(programs=publiserte_programmer))
 
 
