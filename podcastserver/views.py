31abbc39d360a92b17da2e82d40962e05dde385a
Markus Pedersen <markus@markus2.com>
2019-02-23 16:27:00 +0100

    Changed typo

--- podcastserver/views.py
+++ podcastserver/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from podgen import Podcast, Episode, Media, Category, Person
 from .util import mp3url, digas2pubdate, guid, feed_url
 from django.conf import settings
-from django.util import timezone
+from django.utils import timezone
 
 
 def srib_admin(request):
@@ -62,12 +62,6 @@ def rssfeed(request, programid):
     # constance_config table when we start on scratch
     # or set up in a new environment.
     from .models import globalsettings
-
-    if programinfo.name == "Assemble":
-        pm = "DETTE ER PROGRAMNAMN"
-    else:
-        pm = programinfo.name
-
 
     p = Podcast(
         name=pm,
